ecbf2f38fa2df6b4bd20e90323a80961ff55cb4b
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-03-08 20:48:50 +0100

    Basic pipeline implemented for classification

--- wsi-cls/src/wsi_cls/pipelines/data_processing/pipeline.py
+++ wsi-cls/src/wsi_cls/pipelines/data_processing/pipeline.py
@@ -1,28 +1,47 @@
 from kedro.pipeline import Node, Pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import enc_target, increment_feature_engineering, correlated_columns_cleanup
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
+            # Node(
+            #     func=preprocess_companies,
+            #     inputs="companies",
+            #     outputs="preprocessed_companies",
+            #     name="preprocess_companies_node",
+            # ),
+            # Node(
+            #     func=preprocess_shuttles,
+            #     inputs="shuttles",
+            #     outputs="preprocessed_shuttles",
+            #     name="preprocess_shuttles_node",
+            # ),
+            # Node(
+            #     func=create_model_input_table,
+            #     inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
+            #     outputs="model_input_table",
+            #     name="create_model_input_table_node",
+            # ),
             Node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
+                func=enc_target,
+                inputs="ortodoncja",
+                outputs="ortodoncja_target_enc",
+                name="enc_target_node",
             ),
             Node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
+                func=increment_feature_engineering,
+                inputs="ortodoncja_target_enc",
+                outputs="ortodoncja_increment_feat_eng",
+                name="increment_feature_engineering_node",
             ),
             Node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
-            ),
+                func=correlated_columns_cleanup,
+                inputs=["ortodoncja_increment_feat_eng",
+                        "params:correlated_columns_cleanup_params"],
+                outputs="ortodoncja_corr_cols_cleanup",
+                name="correlated_columns_cleanup_node",
+            )
         ]
     )
