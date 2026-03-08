9d8c56f848ad9caa75b032e3c7d78707ba280898
xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
2026-03-08 20:52:12 +0100

    Ortho exp (#2)
    * Initial ortho exploration
    
    * Basic pipeline implemented for classification

--- wsi-cls/src/wsi_cls/pipelines/data_processing/nodes.py
+++ wsi-cls/src/wsi_cls/pipelines/data_processing/nodes.py
@@ -1,68 +1,126 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
-def _is_true(x: pd.Series) -> pd.Series:
-    return x == "t"
+#
+#
+# def _is_true(x: pd.Series) -> pd.Series:
+#     return x == "t"
+#
+#
+# def _parse_percentage(x: pd.Series) -> pd.Series:
+#     x = x.str.replace("%", "")
+#     x = x.astype(float) / 100
+#     return x
+#
+#
+# def _parse_money(x: pd.Series) -> pd.Series:
+#     x = x.str.replace("$", "").str.replace(",", "")
+#     x = x.astype(float)
+#     return x
+#
+#
+# def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
+#     """Preprocesses the data for companies.
+#
+#     Args:
+#         companies: Raw data.
+#     Returns:
+#         Preprocessed data, with `company_rating` converted to a float and
+#         `iata_approved` converted to boolean.
+#     """
+#     companies["iata_approved"] = _is_true(companies["iata_approved"])
+#     companies["company_rating"] = _parse_percentage(companies["company_rating"])
+#     return companies
+#
+#
+# def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
+#     """Preprocesses the data for shuttles.
+#
+#     Args:
+#         shuttles: Raw data.
+#     Returns:
+#         Preprocessed data, with `price` converted to a float and `d_check_complete`,
+#         `moon_clearance_complete` converted to boolean.
+#     """
+#     shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
+#     shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
+#     shuttles["price"] = _parse_money(shuttles["price"])
+#     return shuttles
+#
+#
+# def create_model_input_table(
+#     shuttles: pd.DataFrame, companies: pd.DataFrame, reviews: pd.DataFrame
+# ) -> pd.DataFrame:
+#     """Combines all data to create a model input table.
+#
+#     Args:
+#         shuttles: Preprocessed data for shuttles.
+#         companies: Preprocessed data for companies.
+#         reviews: Raw data for reviews.
+#     Returns:
+#         Model input table.
+#
+#     """
+#     rated_shuttles = shuttles.merge(reviews, left_on="id", right_on="shuttle_id")
+#     rated_shuttles = rated_shuttles.drop("id", axis=1)
+#     model_input_table = rated_shuttles.merge(
+#         companies, left_on="company_id", right_on="id"
+#     )
+#     model_input_table = model_input_table.dropna()
+#     return model_input_table
+
+def enc_target(df: pd.DataFrame) -> pd.DataFrame:
+    enc = LabelEncoder()
+    enc.fit(df[["growth direction"]])
+    df["growth direction"] = enc.transform(df[["growth direction"]])
+
+    return df
+
+def increment_feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
+    all_columns: list[str] = list(df.columns)
+
+    nine_y_columns = [column for column in all_columns if column.startswith("9_")]
+    twelve_y_columns = [column for column in all_columns if column.startswith("12_")]
+
+    parameter_names = [column[2:] for column in nine_y_columns]
+
+    for parameter_name in parameter_names:
+        df[parameter_name + "_diff"] = df["12_" + parameter_name] - df["9_" + parameter_name]
+        df[parameter_name + "_rat"] = df["12_" + parameter_name] / df["9_" + parameter_name]
+
+    df = df.drop(columns=twelve_y_columns)
+    return df
 
 
-def _parse_percentage(x: pd.Series) -> pd.Series:
-    x = x.str.replace("%", "")
-    x = x.astype(float) / 100
-    return x
+def correlated_columns_cleanup(df: pd.DataFrame, params: dict) -> pd.DataFrame:
+    target_column_name = params["target_column_name"]
+    threshold = params["threshold"]
 
+    corr_matrix = df.corr(numeric_only=True)
 
-def _parse_money(x: pd.Series) -> pd.Series:
-    x = x.str.replace("$", "").str.replace(",", "")
-    x = x.astype(float)
-    return x
+    columns = list(corr_matrix.columns)
+    columns.remove(target_column_name)
+    columns_len = len(columns)
 
+    columns_to_remove = []
 
-def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
-    """Preprocesses the data for companies.
+    for first_column_idx in range(0, columns_len):
+        for second_column_idx in range(first_column_idx + 1, columns_len):
+            first_column_name = columns[first_column_idx]
+            second_column_name = columns[second_column_idx]
 
-    Args:
-        companies: Raw data.
-    Returns:
-        Preprocessed data, with `company_rating` converted to a float and
-        `iata_approved` converted to boolean.
-    """
-    companies["iata_approved"] = _is_true(companies["iata_approved"])
-    companies["company_rating"] = _parse_percentage(companies["company_rating"])
-    return companies
+            if abs(corr_matrix.loc[first_column_name, second_column_name]) < threshold:
+                continue
 
+            column_name_to_remove = first_column_name if abs(
+                corr_matrix.loc[first_column_name, target_column_name]) > abs(
+                corr_matrix.loc[second_column_name, target_column_name]) else second_column_name
 
-def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
-    """Preprocesses the data for shuttles.
+            columns_to_remove.append(column_name_to_remove)
 
-    Args:
-        shuttles: Raw data.
-    Returns:
-        Preprocessed data, with `price` converted to a float and `d_check_complete`,
-        `moon_clearance_complete` converted to boolean.
-    """
-    shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
-    shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
-    shuttles["price"] = _parse_money(shuttles["price"])
-    return shuttles
+    print("Removing correlated columns: ", columns_to_remove)
 
+    df = df.drop(columns=columns_to_remove)
 
-def create_model_input_table(
-    shuttles: pd.DataFrame, companies: pd.DataFrame, reviews: pd.DataFrame
-) -> pd.DataFrame:
-    """Combines all data to create a model input table.
-
-    Args:
-        shuttles: Preprocessed data for shuttles.
-        companies: Preprocessed data for companies.
-        reviews: Raw data for reviews.
-    Returns:
-        Model input table.
-
-    """
-    rated_shuttles = shuttles.merge(reviews, left_on="id", right_on="shuttle_id")
-    rated_shuttles = rated_shuttles.drop("id", axis=1)
-    model_input_table = rated_shuttles.merge(
-        companies, left_on="company_id", right_on="id"
-    )
-    model_input_table = model_input_table.dropna()
-    return model_input_table
+    return df
--- wsi-cls/src/wsi_cls/pipelines/reporting/pipeline.py
+++ wsi-cls/src/wsi_cls/pipelines/reporting/pipeline.py
@@ -11,20 +11,20 @@ def create_pipeline(**kwargs) -> Pipeline:
     """This is a simple pipeline which generates a pair of plots"""
     return Pipeline(
         [
-            Node(
-                func=compare_passenger_capacity_exp,
-                inputs="preprocessed_shuttles",
-                outputs="shuttle_passenger_capacity_plot_exp",
-            ),
-            Node(
-                func=compare_passenger_capacity_go,
-                inputs="preprocessed_shuttles",
-                outputs="shuttle_passenger_capacity_plot_go",
-            ),
-            Node(
-                func=create_confusion_matrix,
-                inputs="companies",
-                outputs="dummy_confusion_matrix",
-            ),
+            # Node(
+            #     func=compare_passenger_capacity_exp,
+            #     inputs="preprocessed_shuttles",
+            #     outputs="shuttle_passenger_capacity_plot_exp",
+            # ),
+            # Node(
+            #     func=compare_passenger_capacity_go,
+            #     inputs="preprocessed_shuttles",
+            #     outputs="shuttle_passenger_capacity_plot_go",
+            # ),
+            # Node(
+            #     func=create_confusion_matrix,
+            #     inputs="companies",
+            #     outputs="dummy_confusion_matrix",
+            # ),
         ]
     )
